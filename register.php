50e0e6dfd16a2541765b2e0e29177b6883c31b40
Jihaya <36874428+Jihaya@users.noreply.github.com>
2020-02-21 23:41:39 +0700

    add fname and lname in sql

--- register.php
+++ register.php
@@ -3,8 +3,8 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$username = $password = $confirm_password = $fname = $lname ="";
+$username_err = $password_err = $confirm_password_err = $fname_err = $lname_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -42,6 +42,72 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         mysqli_stmt_close($stmt);
     }
 	
+	// Validate fname
+    if(empty(trim($_POST["fname"]))){
+        $fname_err = "Please enter a Frist Name.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE fname = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_fname);
+            
+            // Set parameters
+            $param_fname = trim($_POST["fname"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $fname_err = "This Frist Name is already.";
+                } else{
+                    $fname = trim($_POST["fname"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+	
+	// Validate lname
+    if(empty(trim($_POST["lname"]))){
+        $lname_err = "Please enter a Last Name.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE lname = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_lname);
+            
+            // Set parameters
+            $param_lname = trim($_POST["lname"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $lname_err = "This Last Name is already.";
+                } else{
+                    $lname = trim($_POST["lname"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+	
     // Validate password
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter a password.";     
@@ -62,17 +128,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($fname_err) && empty($lname_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password , fname , lname) VALUES (?, ?, ?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password , $param_fname);
+            mysqli_stmt_bind_param($stmt, "ssss", $param_username, $param_password , $param_fname , $param_lname);
             
             // Set parameters
             $param_username = $username;
+			$param_fname = $fname;
+			$param_lname = $lname;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             
             // Attempt to execute the prepared statement
@@ -123,6 +191,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div> 
+			<div class="form-group <?php echo (!empty($fname_err)) ? 'has-error' : ''; ?>">
+                <label>Frist Name</label>
+                <input type="text" name="fname" class="form-control" value="<?php echo $fname; ?>">
+                <span class="help-block"><?php echo $fname; ?></span>
+            </div> 
+			<div class="form-group <?php echo (!empty($lname_err)) ? 'has-error' : ''; ?>">
+                <label>Last Name</label>
+                <input type="text" name="lname" class="form-control" value="<?php echo $lname; ?>">
+                <span class="help-block"><?php echo $lname; ?></span>
             </div> 
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
