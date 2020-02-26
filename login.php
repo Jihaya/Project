e3621e69569aee1ec59654235d3c36f00901bea0
Jihaya <36874428+Jihaya@users.noreply.github.com>
2020-02-26 20:55:01 +0700

    Add files via upload
    login admin user

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: login.php");
     exit;
 }
  
@@ -12,8 +12,8 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$username = $password = $level ="";
-$username_err = $password_err = $level ="";
+$username = $password ="";
+$username_err = $password_err ="";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password, level FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -52,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $level);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
@@ -62,9 +62,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id; 
                             $_SESSION["username"] = $username;
+                            $_SESSION["level"] = $row["$level"];
+ 
+                            if($level =="A"){ //ถ้าเป็น admin ให้กระโดดไปหน้า admin.php
+ 
+                                Header("Location: admin.php");
+         
+                              }
+                            if ($level =="U"){  //ถ้าเป็น member ให้กระโดดไปหน้า home.php
+ 
+                                Header("Location: home.php");
+         
+                            }
 
-                            // Redirect user to welcome page
-                            header("location: home.php");
+                            // Redirect user to home page
+                            //header("location: home.php");
                         }else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
